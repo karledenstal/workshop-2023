9c14d93787a2c37866ae5c28de6cc83a3df047a5
Karl Edenstål <karl.edenstal@rhyme.se>
2023-03-08 07:55:00 +0100

    added one more challenge

---
+++ challenge-4/instructions.md
@@ -0,0 +1,3 @@
+# Challenge #4: Invertera ett binärt träd
+
+Med koden ni fått tilldelad i start.ts, invertera ett binärt träd.
---
+++ challenge-4/start.ts
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+// input: [4,2,7,1,3,6,9]
+// output: [4,7,2,9,6,3,1]
